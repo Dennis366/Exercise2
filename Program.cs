e4d09d2b528867d2474668736f74a9a529e0acc5
Dennis366 <dennisfellgard@outlook.com>
2026-02-25 09:58:42 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class FibonacciProgram
+{
+    
+    static void PrintFibonacci(int n)
+    {
+        int a = 0, b = 1;
+
+        Console.Write("Fibonacci series: ");
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(a + " ");
+
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+
+        Console.WriteLine();
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter how many Fibonacci numbers to display: ");
+        int n = int.Parse(Console.ReadLine()!);
+
+        PrintFibonacci(n);
+    }
+}
